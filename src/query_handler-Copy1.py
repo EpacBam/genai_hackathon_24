9315cefb56317c20c2e70cabf7e480c6f7191bd0
EpacBam <36044878+EpacBam@users.noreply.github.com>
2024-11-13 15:10:41 +0000

    Update query_handler-Copy1.py

--- src/query_handler-Copy1.py
+++ src/query_handler-Copy1.py
@@ -7,8 +7,7 @@ import json
 with open("data_dictionary.json", "r") as f:
     data_dictionary = json.load(f)
 
-#openai.api_key = "YOUR_OPENAI_API_KEY"  # Set up your API key here
-openai.api_key = "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"  # Set up your API key here
+openai.api_key = "YOUR_OPENAI_API_KEY"  # Set up your API key here
 
 def interpret_question_with_openai(query):
     data_dictionary_str = json.dumps(data_dictionary, indent=2)
